e2e88b202511999076502cd12c366de24e6de567
TuHue <94226385+TuHue@users.noreply.github.com>
2021-12-19 13:01:15 +0700

    create component header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.scss";
 import CRoutes from "./config/Routes";
+import CompHeader from "./components/Header/Header";
 function App() {
   return (
     <BrowserRouter>
       <Route
         render={(props) => (
           <>
+            <CompHeader />
             <CRoutes />
           </>
         )}
---
+++ src/commons/Nav.js
@@ -0,0 +1,14 @@
+export const ComHeaderNav = [
+  {
+    display: "Home",
+    path: "/",
+  },
+  {
+    display: "Movies",
+    path: "/movie",
+  },
+  {
+    display: "TV Series",
+    path: "/tv",
+  },
+];
